7870d17ca25167fb8df446e3671a60e49bed23aa
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2023-11-02 18:00:40 +0100

    grid vuetify

--- pages/fetch.vue
+++ pages/fetch.vue
@@ -7,28 +7,25 @@ console.log(data)
 </script>
 
 <template>
-    <h1>
-        Table
-    </h1>
-    <v-btn to="/" color="secondary" size="large">Back home</v-btn>
-    <div v-if="!pending">
-        <div v-for="(item, index) in data.allArticles" :key="index">
-            <h3>{{ item.title }}</h3>
-            <p>{{ item.content }}</p>
-            <v-btn to="/" color="primary" size="large">Lire l'article</v-btn>
-        </div>
-    </div>
-
-
-    <div v-if="!pending" class="d-flex justify-center ma-4">
-        <v-card width="350" outlined v-for="(item, index) in data.allArticles" :key="index">
-            <v-card-title>{{ item.title }}</v-card-title>
-            <v-card-subtitle>Par Ronan Scotet</v-card-subtitle>
-            <v-card-text>{{ item.content }}</v-card-text>
-            <v-card-actions>
-                <v-btn to="/" color="primary" size="large">Suivre la recette</v-btn>
-            </v-card-actions>
-        </v-card>
-    </div>
+    <v-container class="bg-surface-variant" v-if="!pending">
+        <h1>
+            Mes recettes
+        </h1>
+        <v-row>
+            <v-col cols="12" sm="4" v-for="item in data.allArticles" :key="item.id">
+                <v-card width="350" outlined>
+                    <v-card-title>{{ item.title }}</v-card-title>
+                    <v-btn prepend-icon="mdi-account" variant="text">
+                        Ronan Scotet
+                    </v-btn>
+                    <v-card-text>{{ item.content }}</v-card-text>
+                    <v-card-actions>
+                        <v-btn to="/" color="primary" size="large">Suivre la recette</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-btn to="/" color="secondary" size="large">Back home</v-btn>
+    </v-container>
 </template>
 
