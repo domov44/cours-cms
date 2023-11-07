2d63e59a1019b1cec9e03afbda0116867a025d53
Ronan Scotet <ronanscotet467@gmail.com>
2023-11-07 23:02:08 +0100

    notif

--- components/SharePost.vue
+++ components/SharePost.vue
@@ -1,45 +1,76 @@
 <template>
-    <div>
-      <v-btn @click="openPopup" color="primary" size="large">
-        Partager l'article
-      </v-btn>
-      <v-dialog v-model="popup" max-width="500px">
-        <v-card>
-          <v-card-title>
-            Partager l'article
-            <v-spacer></v-spacer>
-            <v-btn icon @click="closePopup" class="close-button">
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-          </v-card-title>
-          <v-card-text>
-            {{ currentURL }}
-          </v-card-text>
-        </v-card>
-      </v-dialog>
-    </div>
-  </template>
-  <script>
-  export default {
-    data() {
-      return {
-        popup: false,
-      };
+  <div>
+    <v-btn @click="openPopup" color="primary" size="large">
+      Partager l'article
+    </v-btn>
+    <v-dialog v-model="popup" max-width="500px">
+      <v-card>
+        <v-card-title>
+          Partager l'article
+          <v-spacer></v-spacer>
+          <v-btn icon @click="closePopup" class="close-button">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <div>
+          <v-text-field :model-value="currentURL" label="URL" variant="outlined" readonly></v-text-field>
+          <v-btn color="primary" size="large" @click="copyURL">
+            Copier
+          </v-btn>
+        </div>
+      </v-card>
+    </v-dialog>
+    <v-alert v-model="alert" variant="tonal" type="success" border="start" closable class="alert">
+      URL copiée avec succès !
+    </v-alert>
+  </div>
+</template>
+
+<style>
+.alert {
+  z-index: 999999;
+  position: absolute;
+  top: 50px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 400px;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      popup: false,
+      alert: false,
+    };
+  },
+  computed: {
+    currentURL() {
+      return window.location.href;
     },
-    computed: {
-      currentURL() {
-        return window.location.href;
-      },
+  },
+  methods: {
+    openPopup() {
+      this.popup = true;
     },
-    methods: {
-      openPopup() {
-        this.popup = true;
-      },
-      closePopup() {
-        this.popup = false;
-      },
+    closePopup() {
+      this.popup = false;
     },
-  };
-  </script>
-  
-  
+    copyURL() {
+      const urlField = document.querySelector('.v-text-field input');
+      urlField.select();
+      document.execCommand('copy');
+      this.alert = true; // Affiche l'alerte de succès
+
+      // Fermer la popup après avoir copié le texte
+      this.closePopup();
+
+      // Fermer l'alerte après 5 secondes
+      setTimeout(() => {
+        this.alert = false;
+      }, 3000);
+    },
+  },
+};
+</script>
