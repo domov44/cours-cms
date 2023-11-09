e172ea44a0b2e505fd610eb71b33e4179b49df0f
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2023-11-09 15:47:52 +0100

    logo patch

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,7 +1,6 @@
 <template>
-    <div v-if="!pending" style="width:100%;" class="d-flex align-center justify-end">
-        <v-img v-for="logo in data.header" :key="logo.id" class="mx-2" :src="logo.url"
-            max-height="180" max-width="180" contain>
+    <div v-if="!pending" style="width:100%;" class="d-flex align-center">
+        <v-img class="mx-2" :src="data.header.logo.url" :alt="data.header.logo.alt" max-height="180" max-width="180" contain>
         </v-img>
     </div>
 </template>
@@ -11,4 +10,3 @@ import Header from '@/cms/queries/header'
 const { data, pending, error, refresh } = useAsyncQuery(Header)
 console.log(data)
 </script>
-  
