ea7d1801b713004e5d9845ee4531b2309f79ab66
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2023-11-09 20:51:31 +0100

    new card and new intro

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,9 +1,9 @@
 <template v-if="route && !pending">
     <PostHero v-if="data">
         <h1 class="text-h2">{{ data.post.titlePost }}</h1>
-        <div class="text" v-html=data.post.introduction[0].richtext></div>
-        <p class="text">{{ data.post.author }} - {{ data.post.postDate }}</p>
+        <div class="introduction" v-html=data.post.introduction[0].richtext></div>
         <SharePost />
+        <p class="text">{{ data.post.author }} - {{ data.post.postDate }}</p>
         <v-img alt={{data.post.seo[0].seo.image.alt}} :src="data.post.seo[0].seo.image.url" cover>
         </v-img>
     </PostHero>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -21,12 +21,11 @@ console.log(data)
             </h1>
             <div v-if="!pending" class="v-grid">
                 <v-card v-for="posts in data.allPosts" :key="posts.id" width="350" outlined>
-                    <v-img class="align-end text-white" height="200" alt={{posts.seo[0].seo.image.alt}}
+                    <v-img height="200" alt={{posts.seo[0].seo.image.alt}}
                         :src="posts.seo[0].seo.image.url" cover>
-                        <v-card-title>{{ posts.titlePost }}</v-card-title>
                     </v-img>
-                    <v-card-subtitle>{{ posts.postDate }}</v-card-subtitle>
-                    <v-card-text>{{ posts.excerp[0].richtext }}</v-card-text>
+                    <v-card-title>{{ posts.titlePost }}</v-card-title>
+                    <v-card-subtitle>Par {{ posts.author }} le {{ posts.postDate }}</v-card-subtitle>
                     <v-card-actions>
                         <v-btn :to="'/blog/' + posts.postSlug" color="primary" size="large">Lire l'article</v-btn>
                         <v-btn size="small" color="surface-variant" variant="text" icon="mdi-share-variant"></v-btn>
