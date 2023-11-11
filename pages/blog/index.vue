784f63fecc6c1d48268776e53ffdff8398f451db
Ronan Scotet <ronanscotet467@gmail.com>
2023-11-11 14:56:46 +0100

    bug grid

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,14 +2,13 @@
 // SSR
 import Posts from '@/cms/queries/posts'
 const { data, pending, error, refresh } = await useLazyAsyncQuery(Posts)
+console.log(data)
 </script>
 <template>
     <Hero>
         <div style="width:80%;">
-            <v-chip class="ma-2" color="primary">
-                Alors quoi de neuf ?
-            </v-chip>
             <h1 class="text-h2 font-weight-bold mb-4">Le blog connecté</h1>
+            <p class="text-body-1 mb-4">Quoi de neuf dans les technos web ?</p>
         </div>
         <div style="width:20%;">
             <img style="width:100%;" src="https://www.datocms-assets.com/110963/1699538708-earth.webp">
@@ -20,8 +19,8 @@ const { data, pending, error, refresh } = await useLazyAsyncQuery(Posts)
             <h1>
                 Les articles
             </h1>
-            <div class="v-grid">
-                <v-card v-if="!pending" v-for="posts in data.allPosts" :key="posts.id" width="350" outlined>
+            <div v-if="!pending" class="v-grid">
+                <v-card v-for="posts in data.allPosts" :key="posts.id" width="350" outlined>
                     <v-img height="200" alt={{posts.seo[0].seo.image.alt}} :src="posts.seo[0].seo.image.url" cover>
                     </v-img>
                     <v-card-title>{{ posts.titlePost }}</v-card-title>
@@ -34,6 +33,8 @@ const { data, pending, error, refresh } = await useLazyAsyncQuery(Posts)
                         <v-btn size="small" color="surface-variant" variant="text" icon="mdi-share-variant"></v-btn>
                     </v-card-actions>
                 </v-card>
+                <img class="planet-img" src="https://www.datocms-assets.com/110963/1699538721-planet.webp">
+                <img class="controller-img" src="https://www.datocms-assets.com/110963/1699538704-controller.webp">
             </div>
         </div>
     </Hero>
