b2776443ae12b871a2745bd7f2cdb13f1b22392a
Ronan Scotet <ronanscotet467@gmail.com>
2023-11-12 11:51:32 +0100

    reformatage date js natif

--- functions/DatePost.js
+++ functions/DatePost.js
@@ -11,7 +11,6 @@ export function formatPostDate(postDate, format = 'dddd DD MMMM YYYY') {
       return date.toLocaleDateString('fr-FR', { day: 'numeric', month: 'long' });
     case 'DD/MM/YYYY':
       return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-    // Ajoutez d'autres formats si nécessaire
     default:
       return date.toLocaleDateString('fr-FR', options);
   }
