e68f3c92a3637428213d8bbf7afd99c165696d33
Ronan Scotet <ronanscotet467@gmail.com>
2023-11-12 11:49:14 +0100

    retire intl pour du natif

--- functions/DatePost.js
+++ functions/DatePost.js
@@ -1,7 +1,18 @@
 // DatePost.js
-import formatDate from 'intl-dateformat';
-
 export function formatPostDate(postDate, format = 'dddd DD MMMM YYYY') {
   const date = new Date(postDate);
-  return formatDate(date, format, { locale: 'fr' });
+
+  const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+
+  switch (format) {
+    case 'dddd DD MMMM YYYY':
+      return date.toLocaleDateString('fr-FR', options);
+    case 'DD MMMM':
+      return date.toLocaleDateString('fr-FR', { day: 'numeric', month: 'long' });
+    case 'DD/MM/YYYY':
+      return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    // Ajoutez d'autres formats si nécessaire
+    default:
+      return date.toLocaleDateString('fr-FR', options);
+  }
 }
