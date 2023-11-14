06783ccc9c9a73bbef78d879c870756488c20fe6
Ronan Scotet <ronanscotet467@gmail.com>
2023-11-14 17:23:50 +0100

    premier test pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,8 +16,45 @@ export default defineNuxtConfig({
       })
     },
     '@pinia/nuxt',
-    '@nuxtjs/apollo'
+    '@nuxtjs/apollo',
+    '@vite-pwa/nuxt',
   ],
+  pwa: {
+    manifest: {
+      name: "Nexinu blog",
+      short_name: "Nexinu",
+      description: "Nexinu le blog orienté nouvelles technologies", 
+      icons:[
+        {
+          src: 'icons/icon_64x64.png',
+          sizes: '64x64',
+          type: 'image/png',
+        },
+        {
+          src: 'icons/icon_144x144.png',
+          sizes: '144x144',
+          type: 'image/png',
+        },
+        {
+          src: 'icons/icon_192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'icons/icon_512x512.png',
+          sizes: '64x64',
+          type: 'image/png',
+        },
+      ]
+    },
+   workbox :{
+    navigateFallback: '/',
+   },
+   devOptions:{
+    enabled: true,
+    type: "module",
+   }
+  },
   vite: {
     vue: {
       template: {
