11f5791e090df58e2fde58d3b241a277daa17424
Ronan Scotet <ronanscotet467@gmail.com>
2023-11-25 23:48:17 +0100

    responsive menu

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,36 @@
 <template>
+    <v-navigation-drawer v-model="drawer" temporary>
+        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg" title="Nexinu"></v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+            <v-list-item to="/" prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
+            <v-list-item to="/blog/" prepend-icon="mdi-forum" title="Blog" value="about"></v-list-item>
+        </v-list>
+    </v-navigation-drawer>
     <v-app-bar app color="background">
         <v-container class="d-flex justify-space-between align-center">
             <div style="width:100%;">
                 <Logo />
             </div>
-            <MainMenu />
-            <SwitchTheme />
+            <div style="width:100%;" class="d-flex align-center justify-end">
+                <MainMenu />
+                <v-btn class="hidden-md-and-up" color="primary" @click.stop="drawer = !drawer">
+                    Menu
+                </v-btn>
+                <SwitchTheme />
+            </div>
         </v-container>
     </v-app-bar>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: null,
+        }
+    },
+}
+</script>
--- components/MainMenu.vue
+++ components/MainMenu.vue
@@ -1,9 +1,7 @@
 <template>
-  <div v-if="!pending" style="width:100%;" class="d-flex align-center justify-end">
-    <v-btn v-for="menuitem in data.header.menu" :key="menuitem.id" :to="menuitem.url" variant="text" class="mt-2 ma-2" color="primary">
+    <v-btn v-if="!pending" v-for="menuitem in data.header.menu" :key="menuitem.id" :to="menuitem.url" variant="text" class="hidden-sm-and-down mt-2 ma-2" color="primary">
       {{ menuitem.title }}
     </v-btn>
-  </div>
 </template>
 <script setup>
 // SSR
