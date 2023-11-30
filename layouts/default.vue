311d5a9ab9a49313d682759902fa0143afc8dd18
Ronan Scotet <ronanscotet467@gmail.com>
2023-11-30 22:33:20 +0100

    VitePwaManifest

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
     <v-app>
         <Html lang="fr">
+        <VitePwaManifest />
         <Header />
         <v-main class="d-flex flex-column align-center justify-center" style="min-height: 300px;">
             <NuxtPage />
